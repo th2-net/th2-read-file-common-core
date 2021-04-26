b2d8cc851c4cf6382e66cdd226c5e40951bd4fed
Oleg Smirnov <oleg.smirnov@exactprosystems.com>
2021-04-26 21:26:47 +0300

    Add javadoc for the configuration

--- src/main/kotlin/com/exactpro/th2/read/file/common/cfg/CommonFileReaderConfiguration.kt
+++ src/main/kotlin/com/exactpro/th2/read/file/common/cfg/CommonFileReaderConfiguration.kt
@@ -19,8 +19,33 @@ package com.exactpro.th2.read.file.common.cfg
 import java.time.Duration
 
 class CommonFileReaderConfiguration(
+    /**
+     * The timeout since the last file modification
+     * before the reader starts to consider the source as finished (it won't be changes any more).
+     * It allows the [com.exactpro.th2.read.file.common.ContentParser] to parse the data at the end of the file
+     * because it won't change anymore.
+     *
+     * NOTE: if the file is actually changed after that,
+     * the file is the last one for the [com.exactpro.th2.read.file.common.StreamId]
+     * and [leaveLastFileOpen] is enabled the new data will be read
+     */
     val staleTimeout: Duration = Duration.ofSeconds(5),
+
+    /**
+     * The maximum number of messages in a one batch.
+     * If the reader tries to add a new portion of messages
+     * to the batch and the new size is bigger than the [maxBatchSize]
+     * the previous messages will be published before the new messages is added to the batch
+     */
     val maxBatchSize: Int = 100,
+
+    /**
+     * The max delay the reader can delay the publication and accumulate the batch.
+     *
+     * NOTE: the reader published the batches only during update processing.
+     * If the update processing method is not invoked the delayed batches won't be published
+     * util the method is invoked. The actual delay might be bigger.
+     */
     val maxPublicationDelay: Duration = Duration.ofSeconds(1),
 
     /**
